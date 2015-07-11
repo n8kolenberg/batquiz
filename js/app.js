0f189e30793e024476da9e7fbd3766aac80cc905
n8kolenberg <nkolenberg@gmail.com>
2015-07-11 15:14:21 +0100

    There was still a bug with the correctAnswer counter as it was in a nested if. Fixed now. Fixed layout as well to make the whole thing responsive. Cleaned up some text

--- js/app.js
+++ js/app.js
@@ -1,23 +1,94 @@
 $(function() {
 
-$('.description').hide().fadeIn(400);
-$('#answerForm, .question').hide();
+/*Let the intro, instructions fade in together with the button to start game=======*/
+$('.description, .intro, form#next').fadeIn(200);
 
 
-/* ====== Function to add questions to the page ====== */
-function addQuestions () {
-	if(counter < quiz.questions.length) {
+/*Define questions and answers=======*/
+var QuizQuestion = function (question, correct, b, c, d) {
+	this.question = question;
+	this.correct = correct;
+	this.b = b;
+	this.c = c;
+	this.d = d;
+}
+
+var quiz0 = new QuizQuestion("Whose nipples caused controversy when they tried to play you in a movie?",
+	"George Clooney",
+	"Val Kilmer", 
+	"Michael Keaton", 
+	"Christian Bale");
+
+var quiz1 = new QuizQuestion("What's the color of CatWoman's eyes?",
+	"Green", "Blue", "Brown", "Black");
+
+var quiz2 = new QuizQuestion("What was the killing Joke?",
+	"A comic depicting the Joker's story", 
+	"The name of the final plan the Joker used in the Dark Knight", 
+	"A joke told by the Riddler", 
+	"A cartoon about the Joker killing Barbara Gordon");
+
+var quiz3 = new QuizQuestion("Who does Mr. Freeze need those crystals for?",
+	"To revive his wife", 
+	"For his suit to work", 
+	"To use as magnifying glasses for his freeze ray", 
+	"To sell for money");
+
+var quiz4 = new QuizQuestion("Who is Batman?",
+	"I'll never say", "Clark Kent", "Bruce Wayne", "Lex Luthor");
+
+// var quiz6 = new QuizQuestion("What's the color of CatWoman's eyes",
+// 	"Green", "Blue", "Brown", "Black");
+
+// var quiz7 = new QuizQuestion("What's the color of CatWoman's eyes",
+// 	"Green", "Blue", "Brown", "Black");
+
+// var quiz8 = new QuizQuestion("What's the color of CatWoman's eyes",
+// 	"Green", "Blue", "Brown", "Black");
+
+
+// var quiz9 = new QuizQuestion("What's the color of CatWoman's eyes",
+// 	"Green", "Blue", "Brown", "Black");
+
+// var quiz10 = new QuizQuestion("What's the color of CatWoman's eyes",
+// 	"Green", "Blue", "Brown", "Black");
+
+var quizArray = [quiz0, quiz1, quiz2, quiz3, quiz4];
+
+//Function to randomize the location of the questions
+function randomizeQuizes () {
+	quizArray.sort( function() {
+	return 0.5 - Math.random();
+	}); //End sort function
+}//End randomizeQuizes
+
+
+
+/* We need a counter to check which question we're at 
+   and another one for the amount of questions correct=======*/
+var counter = 0;
+var correctAnswers = 0;
+
+
+/* We need a function for adding the quizquestion and answers 
+   to the radios and labels =======*/
+function addQuiz () {
+	if (counter < quizArray.length) {
 		randomizeRadios();
-		$('.question').html(quiz.questions[counter].question);
-		$(radioArray[0]).val(quiz.questions[counter].correct).next().html($(radioArray[0]).val());
-		$(radioArray[1]).val(quiz.questions[counter].wrong1).next().html($(radioArray[1]).val());
-		$(radioArray[2]).val(quiz.questions[counter].wrong2).next().html($(radioArray[2]).val());
-		$(radioArray[3]).val(quiz.questions[counter].wrong3).next().html($(radioArray[3]).val());
+		$('.question').html(quizArray[counter].question);
+		$(radioArray[0]).val(quizArray[counter].correct);
+		$(radioArray[1]).val(quizArray[counter].b);
+		$(radioArray[2]).val(quizArray[counter].c);
+		$(radioArray[3]).val(quizArray[counter].d);
+	} //End if
+	$('input[type="radio"]').each(function(){
+		$(this).next().html($(this).val());
+	});
+}//End addQuiz
 
-	}
-}// End addQuestions		
 
-//Create array of the radio inputs so we can Math.random() their index positions
+/* We need an array of the radio inputs 
+   so we can Math.random() their index positions=======*/
 var radioArray = [$('#a'), $('#b'), $('#c'), $('#d')];
 
 //Function to randomize the location of the radiobutton values
@@ -29,127 +100,89 @@ function randomizeRadios () {
 
 
 
-var counter = 0;
-var correctAnswers = 0;
 
 
-//checkout forEach method
-var quiz = {
-	questions : [
 
-		{
-			question : "Whose nipples caused controversy when they tried to play you in a movie?",
-			correct : "George Clooney",
-			wrong1 : "Michael Keaton",
-			wrong2 : "Val Kilmer",
-			wrong3 : "Christian Bale"
-		},
-
-		{
-			question : "What's the color of CatWoman's eyes",
-			correct : "Green",
-			wrong1 : "Blue",
-			wrong2 : "Brown",
-			wrong3 : "Black"
-		},
-
-		{
-			question : "What was the killing Joke?",
-			correct : "A comic depicting the Joker's story",
-			wrong1 : "The name of the final plan the Joker used in the Dark Knight",
-			wrong2 : "A joke told by the Riddler",
-			wrong3 : "A movie about the Joker killing Barbara Gordon"
-		},
-
-		{
-			question : "Who does Mr. Freeze need those crystals for?",
-			correct : "To revive his wife",
-			wrong1 : "For his suit to work",
-			wrong2 : "To wear on his suit",
-			wrong3 : "To use as magnifying glasses for his freeze ray"
-		}
-
-	] //End question array within quiz object
-}; //End quiz object
+/*Allow user to click on button and have quiz appear=======*/
+$('form#next').submit(function(){
+	event.preventDefault();
+	// randomizeQuizes();
+	$('input[type="radio"]').removeAttr('checked');
+	
+	if($('.middle').find('p').is(':visible')) {
+		$('.instructions, .intro, form#next').fadeOut(function(){
+			$('.videoHolder').html(''); //to remove video upon restarting
+			counter = 0;//To be able to add the first question in the array next time
+			correctAnswers = 0;
+			addQuiz();
+			$('#answerForm, .question').fadeIn(300);
+			$('.intro').html('Remember to put a <span class="laugh">SMILE</span> on your face!').fadeIn(200);
+		});//End fadeOut
+	} //End nested if
+	console.log("Counter upon submit: " + counter);
+	console.log("correctAnswers upon submit: " + correctAnswers);
+}); //End submit
 
 
-/* ====== When user clicks on button to move to the next question ======*/
-	$('#next').submit(function(event){
-		event.preventDefault();
-		//Fade out the initial instructions (if not hidden already) 
-		//and then fade in the question form
-		if($('.middle').find('p').is(':visible')) {
-			$('.middle').find('p').fadeOut(200, function(){
-				$('#answerForm, .question').fadeIn(300);
-				counter = 0;
-				correctAnswers = 0;
-				$('.videoHolder').html('');
-				addQuestions();
-			}); //End fadeOut
-		} //End if statement
 
-		$(this).fadeOut(300);
-			$('h1.intro').fadeOut(100, function() {
-				$(this).html('Remember to put a <span class="laugh">SMILE</span> on your face!')
-					   .fadeIn(200);	 
-			});	//End Change of h1 intro text			 
-	});//End submit function
-		
-
-
-/* ====== When user clicks on one of the answers ======*/
-	$('#answerForm').on('click', 'input[type="radio"]', function(){
-		if(counter < quiz.questions.length) {
-			if( $(this).val() === quiz.questions[counter].correct ) {
-				correctAnswers++;
-				console.log(correctAnswers);
-			}
-			// } else {
-			// 	$('')
-		}//End nested if statement
-			
+/*Allow user to choose answer and then move on to next question=======*/
+$('#answerForm').on('click', 'input[type="radio"]', function(){
+	if($(this).val() === quizArray[counter].correct) {
+			correctAnswers++;
+		}//End nested if
 		counter++;
-
-		//We show user for a split second what the correct answer is
-		$(radioArray[0]).next().addClass('correct')
-			
-		setTimeout(function(){
+	
+	if(counter < quizArray.length) {	
+		/*Show user correct answer in split second=======*/
+		$(radioArray[0]).next().addClass('correct');	
+		
+		console.log("counter so far: " + counter);
+		console.log("correctAnswers so far: " + correctAnswers)
+		setTimeout( function(){
 			$(radioArray[0]).next().removeClass('correct');
-		}, 300);
+			$('input[type="radio"]').removeAttr('checked');
+			addQuiz();
+		}, 300)
 
-		//Bring in the new questions
-		setTimeout(function(){ 
-			addQuestions();
-		}, 500);
 
-		//We remove the clicked answer styling after 200 ms
-		setTimeout(function() { $('input[type="radio"').removeAttr('checked')}, 200);
+	} else {
+		showResults();
+	}//End if statement
+}); //End on click
 
-		if(counter == quiz.questions.length) {
-				if(correctAnswers >= 4) {
-					$('#answerForm, .question').fadeOut(400, function() {
-						$('.question').html("<p><span class='laugh'> HA HA HA HA</span> Well done Bats! " + correctAnswers + " answers correct! Didn't think you had it in you!</p>").fadeIn(400);
-						$(".videoHolder").html(
-							    '<video autoplay>' +
-							        '<source src="img/batmanrevised.mp4" type="video/mp4"></source>' +
-							    '</video>');
-						$('#next').fadeIn().find('input').val('Try again');
-					}); //End fadeOut last question
-				} else {
 
-					$('#answerForm, .question').fadeOut(400, function() {
-						$('.question').html("<p>Aaaaww  Batsy... I thought I was dealing with a worthy opponent.. " + correctAnswers + " correct? That's just disappointing.. Oh Well... Bombs Away! <span class='laugh'>HA HA HA HA HA HA!</span></p>").fadeIn(400);
+
+
+
+/*Show user correct amount of answers and try again button=======*/
+function showResults() {	
+		if(correctAnswers >= 4) {
+			$('#answerForm, .question').fadeOut(400, function() {
+							$('.question').html("<p><span class='laugh'> HA HA HA HA</span> Well done Bats! " + correctAnswers + " answers correct! Didn't think you had it in you!</p>").fadeIn(400);
 							$(".videoHolder").html(
-							    '<video autoplay>' +
-							        '<source src="img/jokerlaugh.mp4" type="video/mp4"></source>' +
-							    '</video>');
+								    '<video autoplay>' +
+								        '<source src="img/batmanrevised.mp4" type="video/mp4"></source>' +
+								    '</video>');
+							$('#next').fadeIn().find('input').val('Try again');
+						}); //End fadeOut last question
+					
+					} else {
 
-						$('#next').fadeIn().find('input').val('Try again');
-					});
-				} //End second nested if statement
-			} //End  big if statement
-	}); //End on click
+						$('#answerForm, .question').fadeOut(400, function() {
+							$('.question').html("<p>Aaaww  Batsy... I thought you were a worthy opponent.. " + correctAnswers + " correct? That's just disappointing.. Oh Well... Bombs Away! <span class='laugh'>HA HA HA HA HA HA!</span></p>").fadeIn(400);
+								$(".videoHolder").html(
+								    '<video autoplay>' +
+								        '<source src="img/jokerlaugh.mp4" type="video/mp4"></source>' +
+								    '</video>');
 
-}); //End Ready
+							$('#next').fadeIn().find('input').val('Try again');
+					});//End fadeOut
+		}//End nested if
+} //End showResults()
 
 
+
+debugger;
+
+
+}); //End ready
